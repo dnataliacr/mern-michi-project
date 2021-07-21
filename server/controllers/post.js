443e7df0e971e099b6b6dbab5f3fe01495ea7c05
dnataliacr <dnataliacr@gmail.com>
2021-07-21 20:18:46 -0300

    Update post.js

--- server/controllers/post.js
+++ server/controllers/post.js
@@ -16,7 +16,8 @@ export const createPost = async (req, res) => {
   const newPost = new PostMessage(post);
   try {
     await newPost.save();
+    res.status(201).json(newPost)
   } catch (error) {
-    res.status(404).json({ message: error.message });
+    res.status(409).json({ message: error.message });
   }
 };
