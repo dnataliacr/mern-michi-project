df9881cf4011ebfe6294c9d2ec91ba86aaa29be8
dnataliacr <dnataliacr@gmail.com>
2021-07-21 18:52:35 -0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -4,11 +4,21 @@ import mongoose from "mongoose";
 
 const app = express();
 
-express.use(cors());
+app.use(cors());
 
 app.use(express.urlencoded({ limit: "25mb", extended: true }));
-app.use(express.json({"limit": "25mb"}));
+app.use(express.json({ limit: "25mb" }));
 
-const CONNECTION_URL = 'mongodb+srv://Natalia:f1Ng75dlV01u5GWK>@cluster0.9rjgh.mongodb.net/drags?retryWrites=true&w=majority'
+const CONNECTION_URL =
+"mongodb+srv://Natalia:ymyrf8Y9_x!Lea9@cluster0.9rjgh.mongodb.net/drags?retryWrites=true&w=majority"
 
 const PORT = process.env.PORT || 8000;
+
+mongoose
+  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() =>
+    app.listen(PORT, () =>
+      console.log(`Server Running on Port: http://localhost:${PORT}`)
+    )
+  )
+  .catch((error) => console.log(`${error} did not connect`));
