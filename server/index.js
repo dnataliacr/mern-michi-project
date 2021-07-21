80adadc76c11ac33087460ccb5d290920043a3f4
dnataliacr <dnataliacr@gmail.com>
2021-07-21 18:18:57 -0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -5,3 +5,7 @@ import mongoose from "mongoose";
 const app = express();
 
 express.use(cors());
+
+app.use(express.urlencoded({ limit: "25mb", extended: true }));
+app.use(express.json({"limit": "25mb"}));
+
