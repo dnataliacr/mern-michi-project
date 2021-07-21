665ced3cd5727bdd644f80e5cc12f48c75e4b8a9
dnataliacr <dnataliacr@gmail.com>
2021-07-21 20:34:59 -0300

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
 
+import Form from "./components/Form.js";
+import Posts from "./components/Posts.js";
+
 const App = () => {
   return (
     <Container maxWidth="lg">
@@ -8,8 +11,34 @@ const App = () => {
         <Typography variant="h2" align="center">
           michis michis
         </Typography>
-        <img src={} alt="michilogo" height="60"/>
+        <img
+          src={
+            {
+              /*
+               */
+            }
+          }
+          alt="michilogo"
+          height="60"
+        />
       </AppBar>
+      <Grow in>
+        <Container>
+          <Grid
+            container
+            justifyContent="space-between"
+            alignItems="stretch"
+            spacing="4"
+          >
+            <Grid item xs={12} sm={7}>
+              <Posts />
+            </Grid>
+            <Grid item xs={12} sm={7}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
     </Container>
   );
 };
