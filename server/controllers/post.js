35fc3ff1b380a0ff1c9b7bb412c5c7eb147b459b
dnataliacr <dnataliacr@gmail.com>
2021-07-21 20:00:06 -0300

    yay

--- server/controllers/post.js
+++ server/controllers/post.js
@@ -1,3 +1,14 @@
-export const getPost = (req, res) => {
-  res.send("THIS WORKS");
+import PostMessage from "../models/postMessage.js";
+
+export const getPost = async (req, res) => {
+  try {
+    const postMessages = await PostMessage.find();
+    
+
+    res.status(200).json(postMessages);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
 };
+
+export const createPost = (req, res) => {};
