6035617ddd31cab9ce11afcbf34ff699c1b1f24c
dnataliacr <dnataliacr@gmail.com>
2021-07-21 20:18:12 -0300

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,17 @@
 import React from "react";
+import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
 
-const App = () => <h1>App</h1>;
+const App = () => {
+  return (
+    <Container maxWidth="lg">
+      <AppBar position="static" color="inherit">
+        <Typography variant="h2" align="center">
+          michis michis
+        </Typography>
+        <img src={} alt="michilogo" height="60"/>
+      </AppBar>
+    </Container>
+  );
+};
 
 export default App;
