a5a798bc47b2518b7de7078720a81c8f378e9018
dnataliacr <dnataliacr@gmail.com>
2021-07-21 18:32:09 -0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -9,3 +9,6 @@ express.use(cors());
 app.use(express.urlencoded({ limit: "25mb", extended: true }));
 app.use(express.json({"limit": "25mb"}));
 
+const CONNECTION_URL = 'mongodb+srv://Natalia:f1Ng75dlV01u5GWK>@cluster0.9rjgh.mongodb.net/drags?retryWrites=true&w=majority'
+
+const PORT = process.env.PORT || 8000;
