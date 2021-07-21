e0fa1f51bf2590a9c619b9cedd28ed300c5c2151
dnataliacr <dnataliacr@gmail.com>
2021-07-21 20:04:41 -0300

    Update post.js

--- server/controllers/post.js
+++ server/controllers/post.js
@@ -3,7 +3,6 @@ import PostMessage from "../models/postMessage.js";
 export const getPost = async (req, res) => {
   try {
     const postMessages = await PostMessage.find();
-    
 
     res.status(200).json(postMessages);
   } catch (error) {
@@ -11,4 +10,13 @@ export const getPost = async (req, res) => {
   }
 };
 
-export const createPost = (req, res) => {};
+export const createPost = async (req, res) => {
+  const post = req.body;
+
+  const newPost = new PostMessage(post);
+  try {
+    await newPost.save();
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
