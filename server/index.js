552ed8648c29a687fb04113812a94f3bc65abb48
dnataliacr <dnataliacr@gmail.com>
2021-07-22 20:04:00 -0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config({path: './.env'});
 
 import postRouter from "./routes/post.js";
 
@@ -13,13 +16,13 @@ app.use("/posts", postRouter);
 app.use(express.urlencoded({ limit: "25mb", extended: true }));
 app.use(express.json({ limit: "25mb" }));
 
-const CONNECTION_URL =
-  "mongodb+srv://Natalia:ymyrf8Y9_x!Lea9@cluster0.9rjgh.mongodb.net/drags?retryWrites=true&w=majority";
-
 const PORT = process.env.PORT || 8000;
 
 mongoose
-  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() =>
     app.listen(PORT, () =>
       console.log(`Server Running on Port: http://localhost:${PORT}`)
