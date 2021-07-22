1216aa9618c53857df1831d72a3208bcbd6bc9eb
dnataliacr <dnataliacr@gmail.com>
2021-07-21 22:18:19 -0300

    Update posts.js

---
+++ client/src/reducers/posts.js
@@ -0,0 +1,13 @@
+export default  (posts = [], action) => {
+  switch (action.type) {
+    case "FETCH_ALL":
+      return posts;
+
+    case "CREATE":
+      return posts;
+      break;
+
+    default:
+      return posts;
+  }
+};
