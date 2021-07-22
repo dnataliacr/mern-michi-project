8ac06db1b9a241ec693f9b108acf31ae7e2b7494
dnataliacr <dnataliacr@gmail.com>
2021-07-22 20:03:56 -0300

    Update posts.js

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -1,18 +1,26 @@
+import {
+  LIKE,
+  CREATE,
+  UPDATE,
+  DELETE,
+  FETCH_ALL,
+} from "../constants/actionTypes.js";
+
 export default (posts = [], action) => {
   switch (action.type) {
-    case "FETCH_ALL":
+    case FETCH_ALL:
       return action.payload;
 
-    case "CREATE":
+    case CREATE:
       return [...posts, action.payload];
 
-    case "UPDATE":
-    case "LIKE":
+    case UPDATE:
+    case LIKE:
       return posts.map((post) =>
         posts._id === action.payload._id ? action.payload : post
       );
 
-    case "DELETE":
+    case DELETE:
       return posts.filter((post) => post._id !== action.payload);
       break;
 
