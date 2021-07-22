fb5ad9273dc7a63828ce6e2612e294e13640b98e
dnataliacr <dnataliacr@gmail.com>
2021-07-22 18:34:21 -0300

    Update posts.js

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -8,7 +8,7 @@ export const getPosts = () => async (dispatch) => {
 
     dispatch({ type: "FETCH_ALL", payload: data });
   } catch (error) {
-    console.log(error.message);
+    console.log(error);
   }
 };
 
@@ -23,12 +23,22 @@ export const createPost = (post) => async (dispatch) => {
 
 export const updatePost = (id, post) => async (dispatch) => {
   try {
-    const { data } = await updatePost(id, post);
+    const { data } = await api.updatePost(id, post);
 
     dispatch({ type: "UPDATE", payload: data });
-  } catch (error) {}
+  } catch (error) {
+    console.log(error);
+  }
 };
 
-export const deletePost = (post) => async (dispatch) => {};
+export const deletePost = (id) => async (dispatch) => {
+  try {
+    await api.deletePost(id);
+
+    dispatch({ type: "DELETE", payload: id });
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 export const likePost = (post) => async (dispatch) => {};
