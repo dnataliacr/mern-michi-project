f9ee70b0807a3ef8ac98877d03a7624c18addfcb
dnataliacr <dnataliacr@gmail.com>
2021-07-22 17:15:03 -0300

    Update post.js

--- server/controllers/post.js
+++ server/controllers/post.js
@@ -1,3 +1,5 @@
+import mongoose from "mongoose";
+
 import PostMessage from "../models/postMessage.js";
 
 export const getPost = async (req, res) => {
@@ -16,8 +18,22 @@ export const createPost = async (req, res) => {
   const newPost = new PostMessage(post);
   try {
     await newPost.save();
-    res.status(201).json(newPost)
+    res.status(201).json(newPost);
   } catch (error) {
     res.status(409).json({ message: error.message });
   }
+};
+
+export const updatePost = async (req, res) => {
+  const { id: _id } = req.params;
+  const post = req.body;
+
+  if (!mongoose.Types.ObjectId.isValid(id))
+    return res.status(404).send("No post with that id");
+
+  const updatePost = await PostMessage.findByIdAndUpdate(id, post, {
+    new: true,
+  });
+
+  res.json(updatePost);
 };
