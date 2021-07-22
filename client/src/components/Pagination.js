a2cede3aeb5d873136d5f6af907bbb5b16155d28
dnataliacr <dnataliacr@gmail.com>
2021-07-22 16:21:58 -0300

    Create Pagination.js

---
+++ client/src/components/Pagination.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Pagination, PaginationItem } from "@material-ui/lab";
+
+import styles from "./styles";
+
+const Paginate = () => {
+  const classes = useStyles();
+
+  return <Pagination />;
+};
