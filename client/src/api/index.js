4dc63668c8bbe1b8e3d2bca002f7be3532cbab77
dnataliacr <dnataliacr@gmail.com>
2021-07-22 18:34:24 -0300

    Update index.js

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -4,5 +4,6 @@ const url = "http://localhost:8000/posts";
 
 export const fetchPosts = () => axios.get(url);
 export const createPost = (newPost) => axios.post(url, newPost);
-export const updatePost = (id, updatePost) =>
-  axios.patch(`${url}/${id}`, updatePost);
+export const updatePost = (id, updatedPost) =>
+  axios.patch(`${url}/${id}`, updatedPost);
+  export const deletePost = (id) => axios.delete(`${url}/${id}`)
