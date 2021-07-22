6e9bcbcc48df9ed7ee1044e78d7a134b0ed4ce72
dnataliacr <dnataliacr@gmail.com>
2021-07-21 23:01:55 -0300

    Update posts.js

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -2,7 +2,12 @@ import * as api from "../api";
 
 //action creators
 
-const getPosts = () => async (dispatch) => {
-  const action = { type: "FETCH_ALL", payload: [] };
-  dispatch(action);
+export const getPosts = () => async (dispatch) => {
+  try {
+    const { data } = await api.fetchPosts();
+
+    dispatch({ type: "FETCH_ALL", payload: data });
+  } catch (error) {
+    console.log(error.message);
+  }
 };
