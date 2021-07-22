39b3a14dde014ee792f6e68ca1bfcbd3f8cf1c15
dnataliacr <dnataliacr@gmail.com>
2021-07-22 17:45:41 -0300

    Update posts.js

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -5,6 +5,11 @@ export default (posts = [], action) => {
 
     case "CREATE":
       return [...posts, action.payload];
+
+    case "UPDATE":
+      return posts.map((post) =>
+        posts.id == action.payload.id ? action.payload : post
+      );
       break;
 
     default:
