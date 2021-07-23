4102e8d015a7131fa1756ba7807dc3fc60f9d23c
dnataliacr <dnataliacr@gmail.com>
2021-07-23 11:26:21 -0300

    wpi

--- client/src/constants/actionTypes.js
+++ client/src/constants/actionTypes.js
@@ -1,5 +1,5 @@
-export const LIKE = "LIKE";
-export const CREATE = "CREATE";
-export const DELETE = "DELETE";
-export const UPDATE = "UPDATE";
-export const FETCH_ALL = "FETCH_ALL";
+export const CREATE = 'CREATE';
+export const UPDATE = 'UPDATE';
+export const DELETE = 'DELETE';
+export const FETCH_ALL = 'FETCH_ALL';
+export const LIKE = 'LIKE';
