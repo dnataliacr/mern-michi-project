9830ff78a3f371f682b7d13bbe9950bb8a133018
dnataliacr <dnataliacr@gmail.com>
2021-07-24 18:12:00 -0300

    Update styles.js

--- client/src/components/Posts/Post/styles.js
+++ client/src/components/Posts/Post/styles.js
@@ -17,7 +17,7 @@ export default makeStyles({
     display: "flex",
     flexDirection: "column",
     justifyContent: "space-between",
-    borderRadius: "15px",
+
     height: "100%",
     position: "relative",
   },
@@ -45,7 +45,7 @@ export default makeStyles({
     padding: "0 16px",
   },
   cardActions: {
-    padding: "0 16px 8px 16px",
+    padding: "0 16px 4px 16px",
     display: "flex",
     justifyContent: "space-between",
   },
