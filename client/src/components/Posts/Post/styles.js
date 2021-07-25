e6255e50fede2802cb619b62a28b7eb77c16f628
dnataliacr <dnataliacr@gmail.com>
2021-07-25 01:53:37 -0300

    Update styles.js

--- client/src/components/Posts/Post/styles.js
+++ client/src/components/Posts/Post/styles.js
@@ -4,8 +4,11 @@ export default makeStyles({
   media: {
     height: 0,
     paddingTop: "56.25%",
-    backgroundColor: "rgba(0, 0, 0, 0.5)",
+    backgroundColor: "rgba(0, 0, 0, 0.3)",
     backgroundBlendMode: "darken",
+    "&:hover": {
+      backgroundColor: "rgba(0, 0, 0, 0.0)",
+    },
   },
   border: {
     border: "solid",
@@ -25,13 +28,12 @@ export default makeStyles({
     position: "absolute",
     top: "20px",
     left: "20px",
-    color: "white",
+    color: "#fff",
   },
   overlay2: {
     position: "absolute",
     top: "20px",
     right: "20px",
-    color: "white",
   },
   grid: {
     display: "flex",
