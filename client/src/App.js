090e68a8b704263ae85264cd2b5da7717a82822f
dnataliacr <dnataliacr@gmail.com>
2021-07-25 01:56:29 -0300

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,13 @@
 import React, { useState, useEffect } from "react";
-import { createTheme, ThemeProvider, Toolbar } from "@material-ui/core";
-import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
-
+import {
+  Container,
+  AppBar,
+  Typography,
+  Grow,
+  Grid,
+  createTheme,
+  ThemeProvider,
+} from "@material-ui/core";
 import { useDispatch } from "react-redux";
 
 import Posts from "./components/Posts/Posts";
@@ -9,14 +15,7 @@ import Form from "./components/Form/Form";
 import { getPosts } from "./actions/posts";
 import useStyles from "./styles";
 
-const theme = createTheme({
-  typography: {
-    fontFamily: "Quicksand",
-    fontWeightLight: 400,
-    fontWeightBold: 800,
-  },
-});
-
+import { useSelector } from "react-redux";
 const App = () => {
   const [currentId, setCurrentId] = useState(0);
   const dispatch = useDispatch();
@@ -25,42 +24,39 @@ const App = () => {
   useEffect(() => {
     dispatch(getPosts());
   }, [currentId, dispatch]);
-
+  const posts = useSelector((state) => state.posts);
   return (
- <>
-    
-    <AppBar position="static" className={classes.appBar}>
-  <Toolbar variant="dense">
-
-
-
-    <Typography variant="h3" color="inherit" component="div">
-      michisite
-    </Typography>
-  </Toolbar>
-</AppBar>
-    <Container maxWidth="lg">
-  
-      <Grow in>
-     
-          <Grid
-            container
-            justify="space-between"
-            alignItems="stretch"
-            spacing={3}
-          >
-            <Grid item xs={12} sm={7}>
-              <Posts setCurrentId={setCurrentId} />
+    <>
+      <AppBar className={classes.appBar} position="static" color="transparent">
+        <Typography
+          className={classes.heading}
+          variant="h2"
+          align="center"
+          color="black"
+        >
+          michipost
+        </Typography>
+      </AppBar>
+      <Container maxWidth="lg">
+        <Grow in>
+          <Container>
+            <Grid
+              container
+              justify="space-around"
+              alignItems="stretch"
+              spacing={3}
+            >
+              <Grid item xs={12} sm={9}>
+            <Posts setCurrentId={setCurrentId} /> 
+              </Grid>
+              <Grid item xs={12} sm={3}>
+                <Form currentId={currentId} setCurrentId={setCurrentId} />
+              </Grid>
             </Grid>
-            <Grid item xs={12} sm={4}>
-              <Form currentId={currentId} setCurrentId={setCurrentId} />
-            </Grid>
-          </Grid>
-        </Container>
-      </Grow>
-  
-    </Container>
-  </>
+          </Container>
+        </Grow>
+      </Container>
+    </>
   );
 };
 
