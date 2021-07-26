d74ae0638ac10dfd246c0ec20a25501d03295b93
dnataliacr <dnataliacr@gmail.com>
2021-07-26 13:05:32 -0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -10,18 +10,18 @@ import postRouter from "./routes/post.js";
 
 const app = express();
 
-
 app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
 
 app.use(cors());
 
-
 app.use("/posts", postRouter);
-
 
 const PORT = process.env.PORT || 8000;
 
+app.get("/", (req, res) => {
+  res.send("heroku app");
+});
 mongoose
   .connect(process.env.CONNECTION_URL, {
     useNewUrlParser: true,
