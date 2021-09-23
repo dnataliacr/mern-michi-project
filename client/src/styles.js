d587f40e9be15326d7ccd450468ad4ece96470c3
Natalia <dnataliacr@gmail.com>
2021-09-23 20:32:51 -0300

    add footer styles

--- client/src/styles.js
+++ client/src/styles.js
@@ -1,7 +1,6 @@
 import { makeStyles } from "@material-ui/core/styles";
 
-export default makeStyles((theme
-  ) => ({
+export default makeStyles((theme) => ({
   appBar: {
     margin: "0 0 100px 0",
 
@@ -16,9 +15,36 @@ export default makeStyles((theme
   heading: {
     color: "#fff",
   },
+  footer: {
+    marginTop: "50px",
+    padding: "20px",
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#fff",
+    color: "#000",
+  },
+  anchor: {
+    color: "#000",
+    textDecoration: "none",
+    animation: "beat 2s infinite",
+  },
+  icon: {
+    animation: `$beat 2s infinite`,
+  },
+  '@keyframes beat': {
+    "0%": {
+      tranform: "scale(1)"
+    },
+    "50%": {
+      transform: "scale(0.6)"
+    }
+  },
   image: {
     marginLeft: "15px",
   },
+
   [theme.breakpoints.down("sm")]: {
     mainContainer: {
       flexDirection: "column-reverse",
